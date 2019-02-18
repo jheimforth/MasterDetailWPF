c310a74bf00bccf7ae1058d8da609a85a4df567c
jared heimforth <jheimforth78@live.com>
2019-02-17 20:30:19 -0500

    Add project files.

---
+++ WPF_MasterDetailApp.S1.Str/BusinessLayer/ProductBL.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using WPF_MasterDetailApp;
+using WPF_MasterDetailApp.Models;
+
+namespace WPF_MasterDetailApp.BusinessLayer
+{
+    public class ProductBL
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+        ProductWindowView _productWindowView;
+        ProductWindowPresenter _ProductWindowPresenter;
+
+
+        #endregion
+
+        #region PROPERTIES
+
+        #endregion
+
+        #region CONSTRUCTORS
+        public ProductBL()
+        {
+            _ProductWindowPresenter = new ProductWindowPresenter(GetCompanyData(), GetProductData());
+
+            _productWindowView = new ProductWindowView(_ProductWindowPresenter);
+            _productWindowView.DataContext = _ProductWindowPresenter;
+            _productWindowView.Show();
+        }
+
+
+        #endregion
+
+        #region METHODS
+        private Company GetCompanyData()
+        {
+            return new Company()
+            {
+                Name = "Infinite Music Record Company",
+                Address = "224 8th street",
+                City = "Traverse City"
+            };
+        }
+
+        private Product GetProductData()
+        {
+            return new Product()
+            {
+                AlbumName = "The Discovery",
+                ArtistName = "Born of Osiris",
+                Genre = "Metal",
+                Format = "CD",
+                Runtime = 41,
+                Price = 9.99,
+                ReleaseYear = 2011
+            };
+        }
+
+
+
+        #endregion
+
+        #region EVENTS
+
+
+        #endregion
+
+    }
+}
---
+++ WPF_MasterDetailApp.S1.Str/Models/Product.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_MasterDetailApp.Models
+{
+    public class Product
+    {
+        #region ENUMS
+
+
+
+        #endregion
+
+        #region FIELDS
+        private string _albumName;
+        private string _artistName;
+        private string _genre;
+        private string _format;
+        private int _runtime;
+        private double _price;
+        private int _releaseYear;
+        private string _imageName;
+
+       
+
+
+
+
+
+
+
+
+
+
+
+        #endregion
+
+        #region PROPERTIES
+
+        public string AlbumName
+        {
+            get { return _albumName; }
+            set { _albumName = value; }
+        }
+
+        public string ArtistName
+        {
+            get { return _artistName; }
+            set { _artistName = value; }
+        }
+
+        public string Genre
+        {
+            get { return _genre; }
+            set { _genre = value; }
+        }
+
+        public string Format
+        {
+            get { return _format; }
+            set { _format = value; }
+        }
+
+        public int Runtime
+        {
+            get { return _runtime; }
+            set { _runtime = value; }
+        }
+
+        public double Price
+        {
+            get { return _price; }
+            set { _price = value; }
+        }
+
+        public int ReleaseYear
+        {
+            get { return _releaseYear; }
+            set { _releaseYear = value; }
+        }
+
+        public string ImageName
+        {
+            get { return _imageName; }
+            set { _imageName = value; }
+        }
+
+        #endregion
+
+        #region CONSTRUCTORS
+
+
+
+        #endregion
+
+        #region METHODS
+
+
+        #endregion
+
+        #region EVENTS
+
+
+
+        #endregion
+    }
+}
